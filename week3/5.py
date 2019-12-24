f9a6132ea9436eb891ed83028b4be96b63249af8
cryptolemming <aliayoub22@gmail.com>
2019-12-23 22:18:19 -0500

    week 3 start

--- week3/5.py
+++ week3/5.py
@@ -4,6 +4,7 @@ import sys
 
 def min_trips(entries, segments):
     trips = 1
+    set = [segments[0]]
     segments = sorted(segments, key=lambda t: t[0])
 
     target = max = segments[0][1]
@@ -14,15 +15,19 @@ def min_trips(entries, segments):
             if coord[1] > max: max = coord[1]
         else:
             trips += 1
+            set.append(segments[i])
             if coord[0] <= max:
                 target = max
                 max = coord[1]
             else:
                 target, max = coord[1]
 
-    return trips
+    return { trips: trips, set: set }
 
 if __name__ == '__main__':
     entries = int(sys.stdin.readline())
     segments = [(line[0], line[1]) for line in (list(map(int, line.split())) for line in sys.stdin.read().splitlines())]
-    print(min_trips(entries, segments))
+    result = min_trips(entries, segments)
+    print(result.trips)
+    for coord in result.set:
+        print("{} {}".format(coord[0], coord[1]))
